ff51fb4258ae4f0157a3b41efe4373a4a821a5be
khanhnqse <khanhqn03@gmail.com>
2024-10-09 21:11:31 +0700

    feat: :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { PATHS } from "./constant/path";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import AboutUs from "./pages/AboutUsPage/AboutUsPage";
 import NewsPage from "./pages/News Page/NewsPage";
-
 import ContactUsForm from "./pages/Contact/Contact";
 import ProductPage from "./pages/ProductPage/Product";
 import ProductDetailPage from "./pages/Product Detai Page/ProductDetail";
@@ -14,24 +13,54 @@ import CartPage from "./pages/CartPage/CartPage";
 import ArticleDetail from "./pages/NewsDetailPage/NewsDetail";
 import UserProfile from "./pages/UserProfile/UserProfile";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path={PATHS.HOME} element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path={PATHS.ABOUT_US.INDEX} element={<AboutUs />} />
-        <Route path={PATHS.NEWS.INDEX} element={<NewsPage />} />
-        <Route path={PATHS.CONTACT.INDEX} element={<ContactUsForm />} />
-        <Route path={PATHS.PRODUCTS.INDEX} element={<ProductPage />} />
-        <Route path={PATHS.PRODUCTS.DETAIL} element={<ProductDetailPage />} />
-        <Route path={PATHS.CART.INDEX} element={<CartPage />} />
-        <Route path={PATHS.NEWS.DETAIL} element={<ArticleDetail />} />
-        <Route path={PATHS.PROFILE.INDEX} element={<UserProfile />} />
-        <Route path={PATHS.CHECKOUT.INDEX} element={<CheckoutPage />} />
-      </Route>
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path={PATHS.HOME} element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path={PATHS.ABOUT_US.INDEX} element={<AboutUs />} />
+          <Route path={PATHS.NEWS.INDEX} element={<NewsPage />} />
+          <Route path={PATHS.CONTACT.INDEX} element={<ContactUsForm />} />
+          <Route path={PATHS.PRODUCTS.INDEX} element={<ProductPage />} />
+          <Route path={PATHS.PRODUCTS.DETAIL} element={<ProductDetailPage />} />
+          <Route
+            path={PATHS.CART.INDEX}
+            element={
+              <PrivateRoute>
+                <CartPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path={PATHS.NEWS.DETAIL} element={<ArticleDetail />} />
+          <Route
+            path={PATHS.PROFILE.INDEX}
+            element={
+              <PrivateRoute>
+                <UserProfile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={PATHS.CHECKOUT.INDEX}
+            element={
+              <PrivateRoute>
+                <CheckoutPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path={PATHS.LOGIN} element={<LoginPage />} />
+          <Route path={PATHS.REGISTER} element={<RegisterPage />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
